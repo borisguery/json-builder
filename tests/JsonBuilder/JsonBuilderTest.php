900c3c01c9611f4ba66cf88d85faf14359766910
Boris Guéry <guery.b@gmail.com>
2015-01-12 12:36:34 +0100

    Add empty tests

---
+++ tests/JsonBuilder/JsonBuilderTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Boris Guéry <guery.b@gmail.com>
+ */
+
+namespace JsonBuilder;
+
+class JsonBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuildASimpleJsonObject()
+    {
+    }
+
+    public function testBuildAMoreComplexJsonObject()
+    {
+    }
+
+    public function testBuildASimpleString()
+    {
+    }
+
+    public function testBuildASimpleNumber()
+    {
+    }
+
+    public function testBuildNullValue()
+    {
+    }
+}
